1b4db534f56d832cec843b6e917a3c769a5373fa
Milan Tomic <milan.tomic@computerrock.com>
2017-08-22 15:42:53 +0200

    added Router and 404

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+var createBrowserHistory = require('history/lib/createBrowserHistory');
 
 /*
     App
@@ -12,7 +17,7 @@ var App = React.createClass({
         return(
             <div className="catch-of-the-day">
                 <div className="menu">
-                    <Header/>
+                    <Header tagline="Fresh Seafood Market" num="5000"/>
                 </div>
                 <Order/>
                 <Inventory/>
@@ -28,8 +33,18 @@ var App = React.createClass({
 
 var Header = React.createClass({
    render : function() {
+       console.log(this.props)
+
        return (
-           <p>Header</p>
+           <header className="top">
+               <h1>Catch
+                   <span className="ofThe">
+                       <span className="of">of</span>
+                       <span className="the">the</span>
+                   </span>
+                   Day</h1>
+               <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+           </header>
        );
    }
 });
@@ -80,4 +95,27 @@ var StorePicker = React.createClass({
    }
 });
 
-ReactDOM.render(<App/>, document.querySelector('#main'));
+/*
+    Not Found
+ */
+
+var NotFound = React.createClass({
+   render : function() {
+       return <h1>Not Found!</h1>
+   }
+});
+
+/*
+    Routes
+ */
+
+var routes = (
+    <Router history={createBrowserHistory()}>
+        <Route path="/" component={StorePicker} />
+        <Route path="/store/:storeId" component={App} />
+        <Route path="*" component={NotFound} />
+    </Router>
+)
+
+
+ReactDOM.render( routes , document.querySelector('#main'));
