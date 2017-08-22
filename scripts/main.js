0e505e0e80177c5e945eb4e43a85e7ff975203a5
Milan Tomic <milan.tomic@computerrock.com>
2017-08-22 14:26:36 +0200

    first components made and added gitignore file

---
+++ scripts/main.js
@@ -0,0 +1,83 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+/*
+    App
+ */
+
+
+var App = React.createClass({
+
+    render: function() {
+        return(
+            <div className="catch-of-the-day">
+                <div className="menu">
+                    <Header/>
+                </div>
+                <Order/>
+                <Inventory/>
+            </div>
+        );
+    }
+});
+
+/*
+    Header
+    <Header/>
+ */
+
+var Header = React.createClass({
+   render : function() {
+       return (
+           <p>Header</p>
+       );
+   }
+});
+
+/*
+ Order
+ <Order/>
+ */
+
+var Order = React.createClass({
+    render : function() {
+        return (
+            <p>Order</p>
+        );
+    }
+});
+
+/*
+ Inventory
+ <Inventory/>
+ */
+
+var Inventory = React.createClass({
+    render : function() {
+        return (
+            <p>Inventory</p>
+        );
+    }
+});
+
+/*
+    StorePicker
+    <StorePicker/>
+ */
+
+var StorePicker = React.createClass({
+   render : function() {
+
+       var name = 'wes';
+
+       return (
+           <form className="store-selector">
+               <h2>Please Enter A Store {name}</h2>
+               <input type="text" ref="storeId" />
+                   <input type="Submit" />
+           </form>
+       );
+   }
+});
+
+ReactDOM.render(<App/>, document.querySelector('#main'));
