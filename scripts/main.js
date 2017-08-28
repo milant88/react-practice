8079036c35279b183cff52a694fe6ccfe472b9ad
Milan Tomic <milan.tomic@computerrock.com>
2017-08-28 16:18:12 +0200

    saving data to localStorage and updating FIsh and Order components

--- scripts/main.js
+++ scripts/main.js
@@ -32,6 +32,24 @@ var App = React.createClass({
           context : this,
           state : 'fishes'
       });
+
+      var localStorageRef = localStorage.getItem('order-' + this.props.params.storeId);
+
+      if(localStorageRef) {
+          // update our component state to reflect what is in localStorage
+          this.setState({
+             order : JSON.parse(localStorageRef)
+          });
+      }
+    },
+
+    componentWillUpdate : function(nextProps, nextState) {
+      localStorage.setItem('order-' + this.props.params.storeId, JSON.stringify(nextState.order));
+    },
+
+    addToOrder : function(key) {
+        this.state.order[key] = this.state.order[key] + 1 || 1;
+        this.setState({ order : this.state.order });
     },
 
     addFish : function(fish) {
@@ -49,18 +67,18 @@ var App = React.createClass({
     },
 
     renderFish : function(key) {
-      return <Fish key={key} index={key} details={this.state.fishes[key]} />
+        return <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>
     },
     render: function() {
         return(
             <div className="catch-of-the-day">
                 <div className="menu">
-                    <Header tagline="Fresh Seafood Market" num="5000"/>
+                    <Header tagline="Fresh Seafood Market" />
                     <ul className="list-of-fishes">
                         {Object.keys(this.state.fishes).map(this.renderFish)}
                     </ul>
                 </div>
-                <Order/>
+                <Order fishes={this.state.fishes} order={this.state.order} />
                 <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
             </div>
         );
@@ -73,19 +91,27 @@ var App = React.createClass({
  */
 
 var Fish = React.createClass({
-   render : function() {
-       var details = this.props.details;
-       return (
-         <li className="menu-fish">
-             <img src={details.image} alt={details.name} />
-             <h3 className="fish-name">
-                 {details.name}
-                 <span className="price">{h.formatPrice(details.price)}</span>
-             </h3>
-             <p>{details.desc}</p>
-         </li>
-       );
-   }
+    onButtonClick : function() {
+        console.log("Going to add the fish: ", this.props.index);
+        var key = this.props.index;
+        this.props.addToOrder(key);
+    },
+    render : function() {
+        var details = this.props.details;
+        var isAvailable = (details.status === 'available' ? true : false);
+        var buttonText = (isAvailable ? 'Add To Order' : 'Sold Out!');
+        return (
+            <li className="menu-fish">
+                <img src={details.image} alt={details.name} />
+                <h3 className="fish-name">
+                    {details.name}
+                    <span className="price">{h.formatPrice(details.price)}</span>
+                </h3>
+                <p>{details.desc}</p>
+                <button disabled={!isAvailable} onClick={this.onButtonClick}>{buttonText}</button>
+            </li>
+        )
+    }
 });
 
 /*
@@ -158,14 +184,51 @@ var Header = React.createClass({
  Order
  <Order/>
  */
-
 var Order = React.createClass({
-    render : function() {
+    renderOrder : function(key) {
+        var fish = this.props.fishes[key];
+        var count = this.props.order[key];
+
+        if(!fish) {
+            return <li key={key}>Sorry, fish no longer available!</li>
+        }
+
         return (
-            <p>Order</p>
-        );
+            <li>
+                {count}lbs
+                {fish.name}
+                <span className="price">{h.formatPrice(count * fish.price)}</span>
+            </li>)
+    },
+    render : function() {
+        var orderIds = Object.keys(this.props.order);
+
+        var total = orderIds.reduce((prevTotal, key)=> {
+            var fish = this.props.fishes[key];
+            var count = this.props.order[key];
+            var isAvailable = fish && fish.status === 'available';
+
+            if(fish && isAvailable) {
+                return prevTotal + (count * parseInt(fish.price) || 0);
+            }
+
+            return prevTotal;
+        }, 0);
+
+        return (
+            <div className="order-wrap">
+                <h2 className="order-title">Your Order</h2>
+                <ul className="order">
+                    {orderIds.map(this.renderOrder)}
+                    <li className="total">
+                        <strong>Total:</strong>
+                        {h.formatPrice(total)}
+                    </li>
+                </ul>
+            </div>
+        )
     }
-});
+})
 
 /*
  Inventory
