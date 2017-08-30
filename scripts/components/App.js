86ca04c8322c76ae3e597fd475e0eadd827a193a
Milan Tomic <milan.tomic@computerrock.com>
2017-08-30 16:01:21 +0200

    converting App component to ES6

--- scripts/components/App.js
+++ scripts/components/App.js
@@ -8,23 +8,27 @@ import Fish from './Fish';
 import Order from './Order';
 import Inventory from './Inventory';
 import Catalyst from 'react-catalyst';
+import reactMixin from 'react-mixin';
+import autobind from 'autobind-decorator';
 
 // Firebase
 import Rebase from 're-base';
 var base = Rebase.createClass('https://fish-shop-4ca91.firebaseio.com/');
 
-var App = React.createClass({
 
-    mixins : [Catalyst.LinkedStateMixin],
+@autobind
+class App extends React.Component {
 
-    getInitialState : function() {
-        return {
+    constructor() {
+        super();
+
+        this.state = {
             fishes : {},
             order : {}
         }
-    },
+    }
 
-    componentDidMount : function() {
+    componentDidMount() {
         base.syncState(this.props.params.storeId + '/fishes', {
             context : this,
             state : 'fishes'
@@ -38,51 +42,51 @@ var App = React.createClass({
                 order : JSON.parse(localStorageRef)
             });
         }
-    },
+    }
 
-    componentWillUpdate : function(nextProps, nextState) {
+    componentWillUpdate(nextProps, nextState) {
         localStorage.setItem('order-' + this.props.params.storeId, JSON.stringify(nextState.order));
-    },
+    }
 
-    addToOrder : function(key) {
+    addToOrder(key) {
         this.state.order[key] = this.state.order[key] + 1 || 1;
         this.setState({ order : this.state.order });
-    },
+    }
 
-    removeFromOrder : function(key) {
+    removeFromOrder(key) {
         delete this.state.order[key];
         this.setState({
             order: this.state.order
         });
-    },
+    }
 
-    addFish : function(fish) {
+    addFish(fish) {
         var timestamp = (new Date()).getTime();
         //update the state object
         this.state.fishes['fish-' + timestamp] = fish;
         // set the state
         this.setState({ fishes : this.state.fishes });
-    },
+    }
 
-    removeFish : function(key) {
+    removeFish(key) {
         if(confirm('Are you sure you want to remove this fish?')) {
             this.state.fishes[key] = null;
             this.setState({
                 fishes : this.state.fishes
             });
         }
-    },
+    }
 
-    loadSamples : function() {
+    loadSamples() {
         this.setState({
             fishes : require('../sample-fishes')
         });
-    },
+    }
 
-    renderFish : function(key) {
+    renderFish(key) {
         return <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>
-    },
-    render: function() {
+    }
+    render() {
         return(
             <div className="catch-of-the-day">
                 <div className="menu">
@@ -99,12 +103,14 @@ var App = React.createClass({
                     addFish={this.addFish}
                     loadSamples={this.loadSamples}
                     fishes={this.state.fishes}
-                    linkState={this.linkState}
+                    linkState={this.linkState.bind(this)}
                     removeFish={this.removeFish}
                 />
             </div>
         );
     }
-});
+}
+
+reactMixin.onClass(App, Catalyst.LinkedStateMixin);
 
 export default App;
--- scripts/components/Header.js
+++ scripts/components/Header.js
@@ -5,25 +5,26 @@
 
 import React from 'react';
 
-var Header = React.createClass({
-    render : function() {
-        console.log(this.props)
+class Header extends React.Component {
+    render() {
 
         return (
             <header className="top">
                 <h1>Catch
                     <span className="ofThe">
-                       <span className="of">of</span>
-                       <span className="the">the</span>
-                   </span>
+                           <span className="of">of</span>
+                           <span className="the">the</span>
+                       </span>
                     Day</h1>
                 <h3 className="tagline"><span>{this.props.tagline}</span></h3>
             </header>
         );
-    },
-    propTypes : {
-        tagline : React.PropTypes.string.isRequired
     }
-});
+}
+
+Header.propTypes = {
+    tagline : React.PropTypes.string.isRequired
+}
+
 
 export default Header;
